1807b8a644f4fc532ab77c4411f6ba651b8b8628
Sbongumusa2003 <u22710362@tuks.co.za>
2025-11-01 00:13:19 +0200

    Add project files.

---
+++ Controllers/MaintainController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace u22710362_HW03.Controllers
+{
+    public class MaintainController : Controller
+    {
+        // GET: Maintain
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/ReportController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace u22710362_HW03.Controllers
+{
+    public class ReportController : Controller
+    {
+        // GET: Report
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
